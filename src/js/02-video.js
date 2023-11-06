c92e3946b0a8c52efdc6394714db7a028fd92c21
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-06 16:31:09 +0200

    refactored code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,25 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const localItems = localStorage.getItem(LOCALSTORAGE_KEY);
 
-if (localStorage.getItem("videoplayer-current-time")) {
-  const currentSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-  player.setCurrentTime(currentSeconds).then(function(seconds) {
-    console.log(`Current time of video are ${seconds} seconds`);
-  }).catch(function(err) {
-    switch (err.name) {
-      case 'RangeError':
-        console.log('the time was less than 0 or greater than the video’s duration');
-        break;
-      default:
-        console.log('some other error occurred');
-        break;
-    }
-  });
+if (localItems) {
+  const currentSeconds = JSON.parse(localItems);
+  player.setCurrentTime(currentSeconds);
 }
 
 const onPlay = e => {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
